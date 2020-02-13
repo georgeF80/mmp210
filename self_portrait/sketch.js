2c6d67f94af262e15ff2cf8159d5c8b0a30d4383
BMCC Student <bmccstudent@bmccs-mbp.bmcc.cuny.edu>
2020-02-13 17:50:48 -0500

    selfportriat2

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -8,38 +8,46 @@ function setup(){
 }
 
 function draw() {
+	background(0,191, 255); 
 
-	background(0, 200, 0); 
+	fill('green');
+	ellipse(309, 180, 200);// headShape
 
-	 fill('green');
+	fill('black'); 
+	ellipse(355, 165, 30); // RIGHT EYE
+	ellipse(265, 165, 30); // RIGHT EYE
+	fill('white');
+	ellipse(355, 170, 15, 15); //RIGHT eyes
+	ellipse(265, 170, 15, 15); //left eyes
+	
+	strokeWeight(2);
+	line(309,180, 509, 100);
 
-	ellipse(209, 180, 200);
-	ellipse(220, 160, 20);
-	ellipse(160, 160, 30, 10); //left eyes
+
    		
-   	rectMode(CENTER); // center the mouth 
-   	strokeWeight(2);
-   	fill('blue');
-	rect(200, 220, 100, 20, 10);
-	rect(200, 220, 90, 9, 10);
+   //	rectMode(CENTER); // center the mouth 
+   	//strokeWeight(2);
+   	//fill('lightblue');
+	//rect(200, 220, 100, 20, 10);
+	//rect(200, 220, 90, 9, 10);
 
 
-    arc(300,160, 50, 40,-HALF_PI, HALF_PI);
-    arc(300, 160, 20, 30,-HALF_PI, HALF_PI);
+    //arc(300,160, 50, 40,-HALF_PI, HALF_PI);
+    //arc(300, 160, 20, 30,-HALF_PI, HALF_PI);
     //arc(300,160, 50, 40,-HALF_PI, HALF_PI);
     //arc(300, 160, 20, 30,-HALF_PI, HALF_PI);
    //
 
         //line(150, 70, 170, 100);
-    line(160, 70, 180, 100);
-    line(170, 70, 180, 100);
-    //line(160, 80, 190, 100);
-    line(180, 70, 180, 100);
+    //line(160, 70, 180, 100);
+    //line(170, 70, 180, 100);
+   // line(160, 80, 190, 100);
+    //line(180, 70, 180, 100);
     //line(180, 70, 190, 100);/// FOR THE DIFFERNET HAIR
 
         // fill();
         //noStroke();
-        triangle(180, 190, 190, 160, 200, 190);
+       // triangle(180, 190, 200, 160, 300, 200);
 
         
         
