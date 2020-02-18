be385168980aa0d3a74ae77a4032e96fe32721dd
georgeF80 <60409274+georgeF80@users.noreply.github.com>
2020-02-18 00:37:11 -0500

    changes to portrait

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -9,45 +9,61 @@ function setup(){
 }
 
 function draw() {
-	background(0,191, 255); 
+background(0,191, 255); 
 
+	strokeWeight(1);
+	fill('#a6936c');
+	ellipse(309, 200, 210);// headShape
 
-	fill('green');
-	ellipse(309, 180, 200);// headShape
-
-	fill('black'); 
-	ellipse(355, 165, 30); // RIGHT EYE
-	ellipse(265, 165, 30); // RIGHT EYE
-	fill('white');
-	ellipse(355, 170, 15, 15); //RIGHT eyes
-	ellipse(265, 170, 15, 15); //left eyes
-	
 	strokeWeight(2);
-	line(309,180, 509, 100);
+	fill('#1fbf8c'); 
+	ellipse(350, 170, 65, 35); // RIGHT EYE
+	ellipse(270, 170, 65, 35); // RIGHT EYE
+	fill('black');
+	ellipse(360, 170, 15, 25); //RIGHT eyes
+	ellipse(280, 170, 15, 25); //left eyes
+	
+	//strokeWeight(2);
+ 
+	line(320, 225, 420, 190);//left whisker1
+    line(320, 230, 430, 220); // left whisker2
+    line(320, 235, 415, 245);// left whisker3
+    line(300, 225, 190, 190);
+    line(300, 230, 200, 225);
+    line(300, 235, 200, 245);
 
- //   	strokeWeight(2);
+     strokeWeight(3);
+	line(310,225, 310, 240);//middle line for mouth  
+	line(290,245, 310, 240);//right line for the mouth 
+	line(335,245, 310, 240);// left ling for the mouth 
+    
+    fill('white');
+    strokeWeight(1);
+	ellipse(265, 330, 100, 60); // pow
+	ellipse(365, 330, 100, 60); // pow two
+  	//strokeWeight(2);
  //   	fill('blue');
 	// rect(200, 220, 100, 20, 10);
 	// rect(200, 220, 90, 9, 10);
 
 
-    //arc(300,160, 50, 40,-HALF_PI, HALF_PI);
+    //arc(400,100, 100, 50, 0,-HALF_PI, HALF_PI);
     //arc(300, 160, 20, 30,-HALF_PI, HALF_PI);
     //arc(300,160, 50, 40,-HALF_PI, HALF_PI);
     //arc(300, 160, 20, 30,-HALF_PI, HALF_PI);
    //
 
-        //line(150, 70, 170, 100);
+    //line(150, 70, 170, 100);
     //line(160, 70, 180, 100);
     //line(170, 70, 180, 100);
    // line(160, 80, 190, 100);
     //line(180, 70, 180, 100);
     //line(180, 70, 190, 100);/// FOR THE DIFFERNET HAIR
 
-        // fill();
-        //noStroke();
-       // triangle(180, 190, 200, 160, 300, 200);
+        fill('#b31e43');
+        strokeWeight(2);
+        triangle(310, 225, 270, 200, 340, 200);
 
-        // triangle(180, 190, 190, 160, 200, 190);
+        //triangle(180, 190, 190, 160, 200, 190);
 
 }
